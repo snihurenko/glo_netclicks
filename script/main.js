f25bfecfbdaa22dde1e0cfc734fcd36310cf79c4
Anastasiia Snihurenko <snigurenko.anastasiya@gmail.com>
2020-05-28 13:58:34 +0300

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,9 +1,47 @@
-//menu
-
 const leftMenu = document.querySelector('.left-menu');
 const hamburger = document.querySelector('.hamburger');
 const tvShowsList = document.querySelector('.tv-shows__list');
 const modal = document.querySelector('.modal');
+
+
+const DBService = class {
+    getData = async (url) => {
+        const res = await fetch(url)
+        if (res.ok) {
+            return res.json()
+        } else {
+            throw new Error (`Failed to receive data from ${url}; Error ${res.status}`)
+        }
+    }
+    getTestData = () => {
+        return this.getData('test.json')
+    }
+};
+
+const renderCard = response => {
+
+    response.results.forEach(item => {
+        tvShowsList.textContent='';     //clear film list
+
+        const card = document.createElement('li')
+        card.classList.add('tv-shows__item');
+        card.innerHTML = `
+            <a href="#" class="tv-card">
+            <span class="tv-card__vote">8.1</span>
+            <img class="tv-card__img"
+                src="https://image.tmdb.org/t/p/w185_and_h278_bestv2/o9XEd15zRzef9SlOuJYPdS5HCdK.jpg"
+                data-backdrop="https://image.tmdb.org/t/p/w185_and_h278_bestv2/lXWgVQ1whAEMz4Ju88UyPoveIKD.jpg"
+                alt="Звёздные войны: Войны клонов">
+            <h4 class="tv-card__head">Звёздные войны: Войны клонов</h4>
+            </a>
+        `;
+
+        tvShowsList.append(card);
+        //tvShowsList.insertAdjacentElement('afterbegin', card)
+    });
+};
+
+new DBService().getTestData().then(renderCard);
 
 //open-close menu
 
@@ -34,6 +72,9 @@ leftMenu.addEventListener('click', () => {
 // open modal window
 
 tvShowsList.addEventListener('click', event => {
+
+    event.preventDefault();
+
     const target = event.target;
     const card = target.closest('.tv-card');
 
